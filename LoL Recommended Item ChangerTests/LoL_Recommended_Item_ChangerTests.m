fd1dfd13f1f801939582ee6b282d78f281ad881e
Scott Rice <meris608@gmail.com>
2011-11-06 00:15:36 -0500

    Initial Commit

---
+++ LoL Recommended Item ChangerTests/LoL_Recommended_Item_ChangerTests.m
@@ -0,0 +1,33 @@
+//
+//  LoL_Recommended_Item_ChangerTests.m
+//  LoL Recommended Item ChangerTests
+//
+//  Created by Scott on 11/6/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "LoL_Recommended_Item_ChangerTests.h"
+
+
+@implementation LoL_Recommended_Item_ChangerTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in LoL Recommended Item ChangerTests");
+}
+
+@end
